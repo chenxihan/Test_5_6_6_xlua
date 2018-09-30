4d7c70d9c3156834e9d9f4ab3b41cc2f892ee7ac
chenxihan <39304431+chenxihan@users.noreply.github.com>
2018-09-30 11:42:16 +0800

    [*]移动Tools文件夹的位置

--- XLuaProject/Assets/XLua/Src/Editor/Hotfix.cs
+++ XLuaProject/Assets/XLua/Src/Editor/Hotfix.cs
@@ -1572,7 +1572,7 @@ namespace XLua
             var mono_path = Path.Combine(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName),
                 "Data/MonoBleedingEdge/bin/mono.exe");
 #endif
-            var inject_tool_path = "./Assets/Tools/XLuaHotfixInject.exe";
+            var inject_tool_path = "./Tools/XLuaHotfixInject.exe";
             if (!File.Exists(inject_tool_path))
             {
                 UnityEngine.Debug.LogError("please install the Tools");
